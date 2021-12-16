e8f3df36107d895320b6d49a8dbefecbc1d1f062
Erand <=>
2021-12-16 16:22:55 +0100

    Add project files.

---
+++ Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        //To Do
+    }
+}
